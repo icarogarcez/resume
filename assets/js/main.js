5590dca824562c0a524a7b582ae0b26ac44d56fd
Ícaro <allegarcezz@gmail.com>
2021-08-31 01:02:31 -0300

    Adicionando configurações de js no click do menu.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,27 @@
 /* Mostrar menu */
-
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
 
 /* Remover menu no mobile */
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /* Rolar as seções */
 
