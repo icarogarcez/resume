92644cba0014227fe3ffbac7cbfcf00ed042e155
Ícaro <allegarcezz@gmail.com>
2021-08-30 21:27:01 -0300

    Inicializando o projeto.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Currículo Responsivo
+### Ícaro Garcez CV
+Site para currículo com responsividade, tema light/dark mode 🌗 e opção de exportar em PDF 📄. Projeto feito com HTML, CSS e Javascript. 
+
+Referência: [Bedimcode](https://www.youtube.com/c/Bedimcode)
---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+/* Mostrar menu */
+
+
+/* Remover menu no mobile */
+
+
+/* Rolar as seções */
+
+
+/* Mostrar scroll top */ 
+
+
+/* Tema light/dark */ 
+
+
+/* Tamanho e impressão pdf */ 
+
+
+/* Remover o tamanho depois do download */ 
+
+
+/* Gerar o pdf */ 
+
+// PDF área
+
+
+// HTML/PDF opções
+
+
+// Função para chamar as opções de areaCv e Html2Pdf 
+
+
+// 3 funções são chamadas quando o botão é clicado: 
+
+    // 01. A class .scale-cv é adicionada ao corpo, onde é reduzida o tamanho dos elementos
+
+    // 02. O pdf é gerado
+
+    // 03. A classe .scale-cv é removida do corpo depois de 5 segundos para voltar ao tamanho normal
