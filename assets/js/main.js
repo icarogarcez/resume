a43b6614824271f829a5ddf1237e233ed7925359
Ícaro <allegarcezz@gmail.com>
2021-08-31 21:32:59 -0300

    Implementação do botão de scroll top.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,8 @@ const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
     
-    // Validate that variables exist
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
             nav.classList.toggle('show-menu')
         })
     }
@@ -18,7 +16,6 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
@@ -44,7 +41,11 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 /* Mostrar scroll top */ 
-
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 
 /* Tema light/dark */ 
 
